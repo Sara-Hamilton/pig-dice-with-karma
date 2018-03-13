7c07df0bf49e76c7aa52bcdd9f7559779a362883
Kaila Goff and Sara Hamilton <kaila@>
2018-03-13 10:14:47 -0700

    first passing spec of PLayer

--- spec/pigdice-spec.js
+++ spec/pigdice-spec.js
@@ -1,6 +1,16 @@
+// var Player = require('./../js/player.js').playerModule;
+import { Player } from './../src/player.js';
+import { switchTurn } from './../src/player.js';
+import { roll } from './../src/player.js';
+import { updateScores } from './../src/player.js';
+import { animateDice } from './../src/player.js';
+
 describe('Player', function() {
 
-  it('', function() {
-    expect().toEqual()
+  it('roll generates a number less than 7', function() {
+    var number = roll();
+    expect(number).toBeLessThan(7);
+    expect(number).toBeGreaterThan(0);
+    expect(number).not.toBeNull();
   });
 });
