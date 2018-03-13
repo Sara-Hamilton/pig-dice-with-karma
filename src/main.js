424738deb28d7989440abd80e59974a9f7b3e130
Kaila Goff and Sara Hamilton <kaila@>
2018-03-13 08:25:35 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Player } from './player';
+
+$(document).ready(function() {
+  
+});
---
+++ src/player.js
@@ -0,0 +1,6 @@
+export function Player(name) {
+  this.name = name;
+  this.score = 0;
+  this.running = 0;
+  this.total = 0;
+}
