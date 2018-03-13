aee7d770529b80b1a4551a54e192dc4d98607643
Kaila Goff and Sara Hamilton <sara@>
2018-03-13 10:52:12 -0700

    player prototype specs passing

--- spec/pigdice-spec.js
+++ spec/pigdice-spec.js
@@ -6,7 +6,28 @@ import { updateScores } from './../src/player.js';
 import { animateDice } from './../src/player.js';
 
 describe('Player', function() {
+    var reusablePlayer;
 
+  beforeEach(function() {
+    reusablePlayer = new Player("Dave");
+  });
+
+  it('will show addRunning() adjusting the total score', function() {
+    reusablePlayer.addRunning(5);
+    expect(reusablePlayer.total).toEqual(5);
+    reusablePlayer.addRunning(3);
+    expect(reusablePlayer.total).toEqual(8);
+  });
+
+  it("will show addScore() adjusting total score", function() {
+    reusablePlayer.running = 5;
+    reusablePlayer.addScore();
+    expect(reusablePlayer.total).toEqual(5);
+  });
+
+});
+
+describe('roll', function() {
   it('roll generates a number less than 7', function() {
     var number = roll();
     expect(number).toBeLessThan(7);
